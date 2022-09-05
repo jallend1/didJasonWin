60168361e1382df69a88bd90b3cd46d7a7a7c0d5
Jason Allen <39307371+jallend1@users.noreply.github.com>
2022-09-05 14:51:18 -0700

    Streamlines way desired video is selected

--- src/Components/BackgroundVideo.jsx
+++ src/Components/BackgroundVideo.jsx
@@ -4,30 +4,41 @@ import LoadingVideo from '../images/loading.mp4';
 import NotYetVideo from '../images/notyet.mp4';
 
 const BackgroundVideo = ({ gameResults, isReadyToLoadVideo }) => {
+  const backgroundVideos = {
+    win: VictoryVideo,
+    lose: DefeatVideo,
+    pending: NotYetVideo,
+    loading: LoadingVideo
+  };
+
   return (
-    <div className="background-video">
-      {gameResults === 'win' && (
-        <video autoPlay muted loop>
-          <source src={VictoryVideo} type="video/mp4" />
-        </video>
-      )}
-      {gameResults === 'loss' && (
-        <video autoPlay muted loop>
-          <source src={DefeatVideo} type="video/mp4" />
-        </video>
-      )}
-      {gameResults === 'loading' && (
-        <video autoPlay muted loop>
-          <source src={LoadingVideo} type="video/mp4" />
-        </video>
-      )}
-      {gameResults === 'pending' && (
-        <video autoPlay muted loop>
-          <source src={NotYetVideo} type="video/mp4" />
-        </video>
-      )}
+    <div className="background-video" key={gameResults}>
+      <video autoPlay muted loop>
+        <source src={backgroundVideos[gameResults]} type="video/mp4" />
+      </video>
     </div>
   );
 };
 
 export default BackgroundVideo;
+
+// {gameResults === 'win' && (
+//     <video autoPlay muted loop>
+//       <source src={VictoryVideo} type="video/mp4" />
+//     </video>
+//   )}
+//   {gameResults === 'loss' && (
+//     <video autoPlay muted loop>
+//       <source src={DefeatVideo} type="video/mp4" />
+//     </video>
+//   )}
+//   {gameResults === 'loading' && (
+//     <video autoPlay muted loop>
+//       <source src={LoadingVideo} type="video/mp4" />
+//     </video>
+//   )}
+//   {gameResults === 'pending' && (
+//     <video autoPlay muted loop>
+//       <source src={NotYetVideo} type="video/mp4" />
+//     </video>
+//   )}
