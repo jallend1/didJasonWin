9845f40343bf73b489093df939ed647d029ed890
Jason Allen <39307371+jallend1@users.noreply.github.com>
2022-09-05 15:10:46 -0700

    Adds background video for tie game scenario

--- src/Components/BackgroundVideo.jsx
+++ src/Components/BackgroundVideo.jsx
@@ -2,13 +2,15 @@ import VictoryVideo from '../images/win.mp4';
 import DefeatVideo from '../images/defeat.mp4';
 import LoadingVideo from '../images/loading.mp4';
 import NotYetVideo from '../images/notyet.mp4';
+import DrawVideo from '../images/draw.mp4';
 
 const BackgroundVideo = ({ gameResults, isReadyToLoadVideo }) => {
   const backgroundVideos = {
     win: VictoryVideo,
     lose: DefeatVideo,
     pending: NotYetVideo,
-    loading: LoadingVideo
+    loading: LoadingVideo,
+    draw: DrawVideo
   };
 
   if (isReadyToLoadVideo) {
